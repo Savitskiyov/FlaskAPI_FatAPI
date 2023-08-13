3f7fae9bba658219658e44b2a8a02c2e34b96a0a
savit <Savitskiyov@gmail.com>
2023-08-13 18:45:35 +0300

    ДЗ 4

--- Seminar_3/wsgi.py
+++ Seminar_3/wsgi.py
@@ -1,6 +1,6 @@
-
-from lectures.lecture_3_additional_features_flask.app_09 import app
-
+# from lectures.lecture_3_additional_features_flask.app_09 import app
+from task_4.main import app
+from hw.main import app
 
 if __name__ == "__main__":
     # Режим отладки включён
